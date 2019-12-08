69bbb4701c4a93c1b835bdaa8f77c049bcddaa23
Kelly Jiang <kllyjiang@gmail.com>
2019-12-07 22:00:00 -0600

    Updates

---
+++ precache-manifest.d205a9c65b8a5de87bdffa6573628200.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43bdad47f360d34851dba5064154448c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "b2a21a4c3a8720cc721d",
+    "url": "/clicky-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f81430c047335885112c",
+    "url": "/clicky-game/static/js/2.3bacf698.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.3bacf698.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2a21a4c3a8720cc721d",
+    "url": "/clicky-game/static/js/main.c038fbb0.chunk.js"
+  },
+  {
+    "revision": "b859e8c835d6a54c0ef8",
+    "url": "/clicky-game/static/js/runtime-main.bf081868.js"
+  }
+]);
